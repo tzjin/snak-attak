e240ee3f7b251b2ce562258fc0fe6c38a604c88e
Tony Jin <tzjin@princeton.edu>
2015-03-15 22:03:40 -0400

    Adding more routes

--- server.go
+++ server.go
@@ -12,10 +12,29 @@ func hello(c web.C, w http.ResponseWriter, r *http.Request) {
    fmt.Fprintf(w, "Hello, %s!\n", c.URLParams["name"])
 }
 
+func GET_data(c web.C, w http.ResponseWriter, r *http.Request) {
+   // send data as json
+}
+
+func INC_counter(c web.C, w http.ResponseWriter, r *http.Request) {
+   // access database and incr
+}
+
+func DEC_counter(c web.C, w http.ResponseWriter, r *http.Request) {
+   // access database and decr
+}
+
 func main() {
+   // test code
    goji.Get("/hello/:name", hello)
+
+   // static file serve
    goji.Get("/*", http.FileServer(http.Dir("public")))
-   // create handlers for /api/* calls
+
+   // handlers for /api/* calls
+   goji.Get("/api/get/:food", GET_data)
+   goji.Post("/api/inc/:food", INC_counter)
+   goji.Post("/api/dec/:food", DEC_counter)
    
    goji.Serve()
 }
