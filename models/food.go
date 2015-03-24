63312d2ee54282101aa67b422529fcfa69d030bb
Tony Jin <tzjin@princeton.edu>
2015-03-24 01:52:35 -0400

    Adding err handling

--- models/food.go
+++ models/food.go
@@ -1,23 +1,32 @@
 package models
 
 import (
-   "database/sql"
    "fmt"
    "log"
-   
+   "database/sql"
+
    _ "github.com/lib/pq"
 )
 
 const (
    DB_USER     = "user"
    DB_PASSWORD = "password"
-   DB_NAME     = "test"
+   DB_NAME     = "mydb"
 )
 
 func main() {
+   // user and password unncessary?
    dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
       DB_USER, DB_PASSWORD, DB_NAME)
    db, err := sql.Open("postgres", dbinfo)
    checkErr(err)
    defer db.Close()
+
+   // do stuff
+}
+
+func checkErr(err error, msg string) {
+   if err != nil {
+      log.Panicln(msg, err)
+   }
 }
