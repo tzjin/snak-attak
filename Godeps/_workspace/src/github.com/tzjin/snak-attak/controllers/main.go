6dcca003b493884e9abe4db849d24ad4ccfdea04
Katherine Lee <kate.lee168@gmail.com>
2015-03-30 06:43:22 -0400

    added dependences and Heroku Procfile

---
+++ Godeps/_workspace/src/github.com/tzjin/snak-attak/controllers/main.go
@@ -0,0 +1,145 @@
+package controllers
+
+import (
+   "fmt"
+   "net/http"
+
+   "github.com/zenazn/goji/web"
+
+   "html/template"
+
+   "github.com/golang/glog"
+
+   "github.com/tzjin/snak-attak/helpers"
+   "github.com/tzjin/snak-attak/models"
+   "github.com/tzjin/snak-attak/system"
+)
+
+type MainController struct {
+   system.Controller
+}
+
+func (controller *MainController) hello(c web.C, w http.ResponseWriter, r *http.Request) {
+   fmt.Fprintf(w, "Hello, %s!\n", c.URLParams["name"])
+}
+
+func (controller *MainController) GET_data(c web.C, w http.ResponseWriter, r *http.Request) {
+   // send data as json
+}
+
+func (controller *MainController) INC_counter(c web.C, w http.ResponseWriter, r *http.Request) {
+   // access database and incr
+}
+
+func (controller *MainController) DEC_counter(c web.C, w http.ResponseWriter, r *http.Request) {
+   // access database and decr
+}
+
+// Home page route
+func (controller *MainController) Index(c web.C, r *http.Request) (string, int) {
+   t := controller.GetTemplate(c)
+
+   widgets := helpers.Parse(t, "home", nil)
+
+   // With that kind of flags template can "figure out" what route is being rendered
+   c.Env["IsIndex"] = true
+
+   c.Env["Title"] = "Default Project - free Go website project template"
+   c.Env["Content"] = template.HTML(widgets)
+
+   return helpers.Parse(t, "main", c.Env), http.StatusOK
+}
+
+// Sign in route
+func (controller *MainController) SignIn(c web.C, r *http.Request) (string, int) {
+   t := controller.GetTemplate(c)
+   session := controller.GetSession(c)
+
+   // With that kind of flags template can "figure out" what route is being rendered
+   c.Env["IsSignIn"] = true
+
+   c.Env["Flash"] = session.Flashes("auth")
+   var widgets = controller.Parse(t, "auth/signin", c.Env)
+
+   c.Env["Title"] = "Default Project - Sign In"
+   c.Env["Content"] = template.HTML(widgets)
+
+   return controller.Parse(t, "main", c.Env), http.StatusOK
+}
+
+// Sign In form submit route. Logs user in or set appropriate message in session if login was not succesful
+func (controller *MainController) SignInPost(c web.C, r *http.Request) (string, int) {
+   email, password := r.FormValue("email"), r.FormValue("password")
+
+   session := controller.GetSession(c)
+   dbMap := controller.GetDbMap(c)
+
+   user, err := helpers.Login(dbMap, email, password)
+
+   if err != nil {
+      session.AddFlash("Invalid Email or Password", "auth")
+      return controller.SignIn(c, r)
+   }
+
+   session.Values["UserId"] = user.Id
+
+   return "/", http.StatusSeeOther
+}
+
+// Sign up route
+func (controller *MainController) SignUp(c web.C, r *http.Request) (string, int) {
+   t := controller.GetTemplate(c)
+   session := controller.GetSession(c)
+
+   // With that kind of flags template can "figure out" what route is being rendered
+   c.Env["IsSignUp"] = true
+
+   c.Env["Flash"] = session.Flashes("auth")
+
+   var widgets = controller.Parse(t, "auth/signup", c.Env)
+
+   c.Env["Title"] = "Default Project - Sign Up"
+   c.Env["Content"] = template.HTML(widgets)
+
+   return controller.Parse(t, "main", c.Env), http.StatusOK
+}
+
+// Sign Up form submit route. Registers new user or shows Sign Up route with appropriate messages set in session
+func (controller *MainController) SignUpPost(c web.C, r *http.Request) (string, int) {
+   email, password := r.FormValue("email"), r.FormValue("password")
+
+   session := controller.GetSession(c)
+   dbMap := controller.GetDbMap(c)
+
+   user := models.GetUserByEmail(dbMap, email)
+
+   if user != nil {
+      session.AddFlash("User exists", "auth")
+      return controller.SignUp(c, r)
+   }
+
+   user = &models.User{
+      Username: email,
+      Email:    email,
+   }
+   user.HashPassword(password)
+
+   if err := models.InsertUser(dbMap, user); err != nil {
+      session.AddFlash("Error whilst registering user.")
+      glog.Errorf("Error whilst registering user: %v", err)
+      return controller.SignUp(c, r)
+   }
+
+   session.Values["UserId"] = user.Id
+
+   return "/", http.StatusSeeOther
+}
+
+// This route logs user out
+func (controller *MainController) Logout(c web.C, r *http.Request) (string, int) {
+   session := controller.GetSession(c)
+
+   session.Values["UserId"] = nil
+
+   return "/", http.StatusSeeOther
+}
